d0618b1cd0ceef4c5096369e9d09bae59e343158
pitabashchoudhury <pitabash98@gmail.com>
2022-11-04 16:38:08 +0530

    current location fetch successfully

--- lib/repository/current_location.dart
+++ lib/repository/current_location.dart
@@ -1,20 +1,22 @@
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 
-Future<String> currentLocation() async {
+Future<void> currentLocation() async {
   List<Placemark> placemarks;
   Placemark? place;
-  Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
-      .then((Position position) async {
-    placemarks =
-        await placemarkFromCoordinates(position.latitude, position.longitude);
-    place = placemarks[0];
-  });
+  try {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.best);
 
-  // setState(() {
-  //   _currentAddress =
-  //       "${place.street},${place.locality}, ${place.postalCode}, ${place.country}";
-  // });
-  print(place?.locality);
-  return "${place!.locality}";
+    if (position.altitude != null) {
+      placemarks =
+          await placemarkFromCoordinates(position.latitude, position.longitude);
+      place = placemarks[0];
+    }
+
+    print(place!.locality);
+  } catch (Exception) {
+    print("error in location fetching");
+  }
+  // return "${place!.locality}";
 }
