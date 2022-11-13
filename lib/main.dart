c749866cc05dbe71a29ad430703439cd7a064c1b
pitabashchoudhury <pitabash98@gmail.com>
2022-11-13 15:30:50 +0530

    image picker added

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,74 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-void main() {
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ImagePicker picker = ImagePicker();
+  File? _image;
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'PhotoUpload',
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
-          child: Text(
-            "Wel Come",
+          child: Wrap(
+            direction: Axis.vertical,
+            crossAxisAlignment: WrapCrossAlignment.center,
+            children: <Widget>[
+              const Text(
+                "Wel Come",
+              ),
+              Container(
+                width: 200,
+                height: 200,
+                decoration: BoxDecoration(
+                  color: Colors.red[200],
+                ),
+                child: _image != null
+                    ? Image.file(
+                        _image!,
+                        width: 200.0,
+                        height: 200.0,
+                        fit: BoxFit.fill,
+                      )
+                    : Container(
+                        decoration: BoxDecoration(color: Colors.red[200]),
+                        width: 200,
+                        height: 200,
+                        child: Icon(
+                          Icons.camera_alt,
+                          color: Colors.grey[800],
+                        ),
+                      ),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  XFile? image =
+                      await picker.pickImage(source: ImageSource.gallery);
+                  setState(() {
+                    _image = File(image!.path);
+                  });
+                },
+                child: const Text(
+                  "Upload",
+                ),
+              ),
+            ],
           ),
         ),
       ),
