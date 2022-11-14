72fa0215f8c5d5dd0e9c124cd0c4da1c75321b1b
pitabashchoudhury <pitabash98@gmail.com>
2022-11-14 14:34:48 +0530

    mqtt server connection established

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+import 'package:mqtt_client/mqtt_client.dart';
+import 'package:mqtt_client/mqtt_server_client.dart';
 
-  runApp(const MyApp());
+void main() {
+  runApp(
+    const MyApp(),
+  );
 }
 
 class MyApp extends StatefulWidget {
@@ -17,61 +17,116 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  ImagePicker picker = ImagePicker();
-  File? _image;
+  // final MqttServerClient client =
+  //     MqttServerClient('a1q54soguztrms-ats.iot.us-west-2.amazonaws.com', '');
+
+  String? msg = "not connected";
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'PhotoUpload',
+      title: 'Mqtt',
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Wrap(
-            direction: Axis.vertical,
-            crossAxisAlignment: WrapCrossAlignment.center,
-            children: <Widget>[
-              const Text(
-                "Wel Come",
+        appBar: AppBar(
+          title: const Text(
+            'MQTT Protocol',
+          ),
+          elevation: 0.0,
+          centerTitle: true,
+        ),
+        body: SafeArea(
+          child: Center(
+            child: Container(
+              width: 300,
+              height: 300,
+              color: Colors.amberAccent,
+              child: Column(
+                children: <Widget>[
+                  const SizedBox(
+                    height: 80,
+                  ),
+                  const Text(
+                    'WEl COME',
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextButton(
+                    onPressed: () async {
+                      connect();
+                    },
+                    child: const Text(
+                      'connect',
+                    ),
+                  ),
+                  Text(
+                    msg!,
+                  ),
+                ],
               ),
-              Container(
-                width: 200,
-                height: 200,
-                decoration: BoxDecoration(
-                  color: Colors.red[200],
-                ),
-                child: _image != null
-                    ? Image.file(
-                        _image!,
-                        width: 200.0,
-                        height: 200.0,
-                        fit: BoxFit.fill,
-                      )
-                    : Container(
-                        decoration: BoxDecoration(color: Colors.red[200]),
-                        width: 200,
-                        height: 200,
-                        child: Icon(
-                          Icons.camera_alt,
-                          color: Colors.grey[800],
-                        ),
-                      ),
-              ),
-              ElevatedButton(
-                onPressed: () async {
-                  XFile? image =
-                      await picker.pickImage(source: ImageSource.gallery);
-                  setState(() {
-                    _image = File(image!.path);
-                  });
-                },
-                child: const Text(
-                  "Upload",
-                ),
-              ),
-            ],
+            ),
           ),
         ),
       ),
     );
   }
+}
+
+void connect() async {
+  //'ws://t1ce993a.us-east-1.emqx.cloud',
+  try {
+    var client = MqttServerClient(
+      't1ce993a.us-east-1.emqx.cloud',
+      'web',
+    );
+
+    client.logging(on: false);
+    client.keepAlivePeriod = 60;
+    client.port = 15600;
+    client.onConnected = onConnected;
+    client.onDisconnected = onDisconnected;
+    client.pongCallback = pong;
+
+    //client.setProtocolV311();
+    // final MqttConnectMessage connMess = MqttConnectMessage().startClean();
+    // client.connectionMessage = connMess;
+    final connMess = MqttConnectMessage()
+        .authenticateAs('pitabashc98', 'pitabashc98')
+        .startClean()
+        .withWillQos(MqttQos.atLeastOnce);
+    print('EXAMPLE::Mosquitto client connecting....');
+
+    client.connectionMessage = connMess;
+
+    try {
+      await client.connect();
+    } on Exception catch (e) {
+      print('EXAMPLE::client exception - $e');
+      client.disconnect();
+    }
+    if (client.connectionStatus!.state == MqttConnectionState.connected) {
+      print("Connected to AWS Successfully!");
+    } else {
+      print("failed");
+    }
+
+    const topic = 'test';
+    client.subscribe(topic, MqttQos.atMostOnce);
+  } catch (e) {
+    print(e.toString());
+  }
+
+  // return "bubu";
+}
+
+void onConnected() {
+  print("connection successful");
+}
+
+void onDisconnected() {
+  print("disconnected");
+}
+
+void pong() {
+  print("ponged");
 }
